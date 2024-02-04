00fb6f1a9467893428fb3f92fffcc7d443946862
Alinea6 <natalia.grycz28@gmailcom>
2024-02-04 22:38:09 +0100

    order mapper

---
+++ Mapper/OrderMapper.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Mapper;
+
+public class OrderMapper
+{
+    public JObject Map(JArray inputMapping, JObject inputData)
+    {
+        JArray processedMappings = new JArray();
+        var output = new JObject();
+
+        var input = ConvertInputData(inputData);
+        
+        
+        foreach (var mapping in inputMapping)
+        {
+            processedMappings = new JArray(processedMappings.Union(mapping["map"] as JArray));
+        }
+        
+        foreach (var mapping in processedMappings)
+        {
+            // checks for constant values in input mapping and adds it to the output
+            if (mapping.SelectToken("input.type")!=null)
+            {
+                output = AddToJObject(output, mapping.SelectToken("target")?.ToString(),
+                    mapping.SelectToken("input.value")?.ToString());
+            }
+            
+            // maps values from input data to output based on the input mapping properties
+            if (!mapping.SelectToken("input").Children<JObject>().Properties().Any() 
+                && input.Keys.Any(x => x == mapping.SelectToken("input")?.ToString()))
+            {
+                output = AddToJObject(output, mapping.SelectToken("target")?.ToString(),
+                    input[mapping.SelectToken("input")?.ToString()]);
+            }
+        }
+        return output;
+    }
+
+    /// <summary>
+    /// Adds new entries to output json object based on property format
+    /// </summary>
+    /// <returns>input jObject with appended field</returns>
+    private JObject AddToJObject(JObject jObject, string property, string value)
+    {
+        if (property.Contains('.'))
+        {
+            var properties = property.Split(".");
+            jObject.Add(properties[0], new JObject{ [properties[1]] = value });
+            return jObject;
+        }
+        jObject.Add(property, value);
+        return jObject;
+    }
+
+    /// <summary>
+    /// Converted input data to dictionary with keys in the form of `a` or `a.b` and their values being target strings.
+    /// </summary>
+    private Dictionary<string, string> ConvertInputData(JObject inputData)
+    {
+        var inputProperties = inputData.Properties();
+        var inputDictionary = new Dictionary<string, string>();
+        foreach (var property in inputProperties)
+        {
+            var children = property.Children<JObject>().Properties();
+            if (children.Any())
+            {
+                foreach (var childrenProperty in children)
+                {
+                    inputDictionary.Add(string.Join(".", [property.Name, childrenProperty.Name]), childrenProperty.Value.ToString());
+                }
+            }
+            else
+            {
+                inputDictionary.Add(property.Name, property.Value.ToString());
+            }
+        }
+
+        return inputDictionary;
+    }
+}
---
+++ MapperTestProject/OrderMapperTest.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions.Json;
+using Mapper;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace MapperTestProject;
+
+public class OrderMapperTest
+{
+    private OrderMapper _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _sut = new OrderMapper();
+    }
+
+    [Test]
+    public void OrderMapper_should_add_const_values_to_output()
+    {
+        var mapping = JArray.Parse("""
+                                              [
+                                              {
+                                                  "map": [
+                                                      {
+                                                          "target": "smerpConnection.context",
+                                                          "input": {
+                                                              "type": "const",
+                                                              "value": "oms"
+                                                          }
+                                                      }
+                                                  ]
+                                              }
+
+                                              ]
+                                              """);
+
+        var inputData = JObject.Parse("""
+                                      {
+                                      }
+                                      """);
+        var outputData = JObject.Parse("""
+                                        {
+                                           "smerpConnection" : {
+                                               "context" : "oms"
+                                           }
+                                       }
+                                       """);
+
+        var result = _sut.Map(mapping, inputData);
+
+        result.Should().BeEquivalentTo(outputData);
+    }
+
+    [Test]
+    public void OrderMapper_should_map_values_from_input_to_output()
+    {
+        var inputMapping = JArray.Parse("""
+                                        [
+                                        {
+                                            "map": [
+                                                {
+                                                    "target": "platformOrderId",
+                                                    "input": "source.platformOrderId"
+                                                }
+                                            ]
+                                        }
+                                        ]
+                                        """);
+        var inputData = JObject.Parse("""
+                                      {
+                                          "firstName": "test",
+                                          "source" : {
+                                              "platformOrderId" : "abc"
+                                          }
+                                      }
+                                      """);
+        var outputData = JObject.Parse("""
+                                       {
+                                           "platformOrderId" : "abc"
+                                       }
+                                       """);
+
+        var result = _sut.Map(inputMapping, inputData);
+        
+        result.Should().BeEquivalentTo(outputData);
+    }
+}
+                                                                              
+                                                                  
+                                                                  
+                                                             
